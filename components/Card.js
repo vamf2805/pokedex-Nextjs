119341e172b44c620f9b82474a78703de718336c
Victor <vamf2805@gmail.com>
2021-10-31 12:20:19 -0400

    Modificacion de las card pokemon

--- components/Card.js
+++ components/Card.js
@@ -7,12 +7,12 @@ const Card = ({pokemon}) =>{
 				<div className="grid grid-cols-4 gap-5">
 					{pokemon.map((item,index) =>(
 						<Link href={`pokemon/${index + 1}`}>
-							<div className="border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden bg-white shadow-xl cursor-pointer" key={item.name}>
-								<div className="bg-red-500 p-5">
-									<img className="object-cover object-center" src={`${item.url_imagen}.png`} alt={item.name}/>
+							<div className="rounded-xl overflow-hidden shadow-xl cursor-pointer grid grid-cols-1 items-center bg-gray-600" key={item.name}>
+								<div className="flex justify-center py-5">
+									<img className="object-cover object-center w-40" src={`${item.url_imagen}.png`} alt={item.name}/>
 								</div>
 								<div className="my-5 text-center">
-									<h2 className="text-xl capitalize font-bold">{item.name}<span className="text-sm"> #{index + 1}</span></h2>
+									<h2 className="text-2xl text-white capitalize font-bold">{item.name}<span className="text-sm"> #{index + 1}</span></h2>
 								</div>
 							</div>
 						</Link>
