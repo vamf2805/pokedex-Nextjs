18fd374558954a0ae8733d052b065af0bfbddcbc
Victor <vamf2805@gmail.com>
2021-10-31 14:50:46 -0400

    Agregar progress bar a los stats

--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -5,14 +5,14 @@ import Link from "next/link"
 const Pokemon = ({data, pokeId, url_imagen}) =>{
 
     return(
-        <div className="container mx-auto py-20">
+        <div className="container mx-auto py-8">
             <div className="grid grid-cols-3 rounded-lg border-2 border-gray-200">
-                <div className="bg-red-500">
-                    <img src={`${url_imagen}.png`} alt="" />
+                <div className="bg-red-500 flex items-center">
+                    <img className="object-cover object-center" src={`${url_imagen}.png`} alt={data.name} />
                 </div>
-                <div className="border-l col-span-2 text-center bg-gray-600 p-10">
+                <div className="border-l col-span-2 bg-gray-600 p-10">
                     
-                    <h2 className="text-white text-center capitalize text-3xl">{data.name} <span className="text-lg">N.º{pokeId}</span></h2>
+                    <h2 className="text-white text-center font-bold capitalize text-3xl">{data.name} <span className="text-lg">N.º{pokeId}</span></h2>
                     {data.types.length > 1 ? (
                         <p className="text-center text-lg text-white">Tipo: {data.types[0].type.name}/{data.types[1].type.name}</p>
                         
@@ -21,13 +21,31 @@ const Pokemon = ({data, pokeId, url_imagen}) =>{
                     (
                         <p className="text-center text-lg text-white">Tipo: {data.types[0].type.name}</p>
                     )}
-                    <div className="text-center text-lg grid grid-cols-2 text-white gap-5 pt-10">
-                        <p>Hp :{data.stats[0].base_stat}</p>
-                        <p>Ataque: {data.stats[1].base_stat}</p>
-                        <p>Defensa: {data.stats[2].base_stat}</p>
-                        <p>Ataque Especial: {data.stats[3].base_stat}</p>
-                        <p>Defensa Especial: {data.stats[4].base_stat}</p>
-                        <p>Velocidad: {data.stats[5].base_stat}</p>
+                    <div className="text-lg text-white pt-10">
+                        <div>
+                            <p>Hp :{data.stats[0].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[0].base_stat}></progress>
+                        </div>
+                        <div>
+                            <p>Ataque :{data.stats[1].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[1].base_stat}></progress>
+                        </div>
+                        <div>
+                            <p>Defensa :{data.stats[2].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[2].base_stat}></progress>
+                        </div>
+                        <div>
+                            <p>Ataque especial :{data.stats[3].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[3].base_stat}></progress>
+                        </div>
+                        <div>
+                            <p>Defensa especial :{data.stats[4].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[4].base_stat}></progress>
+                        </div>
+                        <div>
+                            <p>Velocidad :{data.stats[5].base_stat}</p>
+                            <progress className="w-full" max="150" value={data.stats[5].base_stat}></progress>
+                        </div>
                     </div>
                     <div className="mt-10">
                         <Link href="/">
