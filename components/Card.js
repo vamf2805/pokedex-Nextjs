a36339907c1eb71663fa806f9f45aba3f62fd993
Victor Marquez <vamf2805@gmail.com>
2022-01-05 14:51:12 -0400

    Agregar Keys al componente Card

--- components/Card.js
+++ components/Card.js
@@ -6,7 +6,7 @@ const Card = ({pokemon}) =>{
 		<section className="text-gray-600 body-font container mx-auto py-20">
 				<div className="grid grid-cols-4 gap-5">
 					{pokemon.map((item,index) =>(
-						<Link href={`pokemon/${index + 1}`}>
+						<Link href={`pokemon/${index + 1}`} key={index}>
 							<div className="rounded-xl overflow-hidden shadow-xl cursor-pointer grid grid-cols-1 items-center bg-gray-600" key={item.name}>
 								<div className="flex justify-center py-5">
 									<img className="object-cover object-center w-40" src={`${item.url_imagen}.png`} alt={item.name}/>
